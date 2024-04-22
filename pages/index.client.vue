c9312b6fb447990cd7e8206e9d720d242c371cbf
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-04-22 09:26:05 +0900

    add pages

---
+++ pages/index.client.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>try <code>?text=foo+bar</code></p>
+        <pre><code>{{ query }}</code></pre>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { query } = useRoute();
+</script>
+
+<style scoped>
+
+</style>
